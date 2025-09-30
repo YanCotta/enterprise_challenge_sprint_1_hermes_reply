102e317ebba14c7db5e37d47d85029ff03b62965
Yan <yanpcotta@gmail.com>
2025-09-30 16:19:59 -0300

    Add extra configuration option to ignore unknown environment variables in settings

--- smart-maintenance-saas/core/config/settings.py
+++ smart-maintenance-saas/core/config/settings.py
@@ -112,6 +112,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         case_sensitive = False
+        extra = "ignore"
 
 
 # Create a global settings instance
